266208d907922abc70d5c2f3191a62d4a12dfc30
Jack McNary <jack.mcnary@gmail.com>
2021-11-08 15:35:17 -0700

    building out engineer prompts

--- index.js
+++ index.js
@@ -66,7 +66,39 @@ function managerQueries() {
         })
 };
 
+function engineerPrompts() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's id?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What's the engineer's github profile?"
+        },
+    ])
 
+
+        .then((answers) => {
+            const manager = new Manager(answers.managerName, answers.mgnrID, answers.managerEmail, answers.managerOfficeNo);
+            generateProfile.createHTML(employeeInfo);
+            console.log("Your team is stacked")
+            promptContinue();
+
+        })
+};
 // and a function to write the README file:
 function writeToFile(data) {
     const testPg = generateProfile(data);
